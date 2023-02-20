a411ca1326420bc78408568698606f14e746c4ca
Damian <damianosk12@interia.pl>
2023-02-20 08:07:04 +0100

    java2

---
+++ java2.js
@@ -0,0 +1,13 @@
+document.querySelector('#nie').addEventListener("click",function tak (){
+    document.getElementById("ok").innerHTML = "<i>Świetnie</i>";
+} )
+
+document.querySelector('No Accesskk').addEventListener("click",function kkk () {
+    document.getElementById("tak").innerText = "<i>Świetnie</i>";
+})
+document.querySelector('#nwm').addEventListener('click', function (){
+    const trzy_elementy = document.querySelector('#nvm');
+    console.log(trzy_elementy)
+    trzy_elementy.querySelector('*')
+
+})
