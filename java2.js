dddad0d84e6c43032c94e8cd9c0e1a6838bf08df
prezioo <damianosk12@interia.pl>
2023-02-20 14:18:19 +0100

    java231

--- java2.js
+++ java2.js
@@ -1,13 +1,18 @@
-document.querySelector('#nie').addEventListener("click",function tak (){
-    document.getElementById("ok").innerHTML = "<i>Świetnie</i>";
-} )
-
-document.querySelector('No Accesskk').addEventListener("click",function kkk () {
-    document.getElementById("tak").innerText = "<i>Świetnie</i>";
-})
-document.querySelector('#nwm').addEventListener('click', function (){
-    const trzy_elementy = document.querySelector('#nvm');
-    console.log(trzy_elementy)
-    trzy_elementy.querySelector('*')
-
-})
+//1
+const jeden = document.querySelector("#jeden");
+jeden.addEventListener("click", function(){
+    document.querySelector("#ha6").innerHTML = "<i>Swietnie</i>";
+});
+console.log(jeden)
+//2
+const dwa =document.querySelector('#dwa');
+dwa.addEventListener("click", function(){
+    document.querySelector("#ha5").innerText = "<i>Swietnie!</i>";
+});
+console.log(dwa);
+//3
+const batont = document.querySelector("#baton3");
+batont.addEventListener("click", function(){
+    const dive =document.querySelectorAll('#diw1');
+    console.log(dive)
+});
