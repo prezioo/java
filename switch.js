db6860e0450d30494d8b447468be6c4e68179592
prezioo <damianosk12@interia.pl>
2023-04-21 10:19:27 +0200

    switch

---
+++ switch.js
@@ -0,0 +1,24 @@
+let miesiac=Math.floor(Math.random()15+1)
+document.querySelector(#pierwszy).innerHTML=miesiac;
+switch (miesiac){
+    case 1:
+    case 2:
+    case 3:
+        document.querySelector(#trzeci).innerHTML="kwartal I";
+        console.log(miesiac)
+        break;
+        let miesiac=Math.floor(Math.random()15+1)
+        document.querySelector(#pierwszy).innerHTML=miesiac;
+    case 4:
+    case 5:
+    case 6:
+        document.querySelector(#trzeci).innerHTML="kwartal II";
+        console.log(miesiac)
+    break;
+        let miesiac=Math.floor(Math.random()15+1)
+        document.querySelector(#pierwszy).innerHTML=miesiac;
+
+
+
+
+}
